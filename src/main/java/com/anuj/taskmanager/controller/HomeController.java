a9d24ac4e1b162b744e94ce36c9a9f446fbac4b1
Anuj Yadav <yadavk.anuj21@gmail.com>
2026-05-02 23:01:32 +0530

    add home endpoint

--- src/main/java/com/anuj/taskmanager/controller/HomeController.java
+++ src/main/java/com/anuj/taskmanager/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Task Manager Backend is Running 🚀";
+        return "Task Manager Backend is LIVE 🚀";
     }
 }
