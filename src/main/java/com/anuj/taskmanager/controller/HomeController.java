2e53af15e52b813d312a84c9700a8d1e0455e8eb
Anuj Yadav <yadavk.anuj21@gmail.com>
2026-05-03 11:54:29 +0530

    serve frontend from static

--- src/main/java/com/anuj/taskmanager/controller/HomeController.java
+++ src/main/java/com/anuj/taskmanager/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Task Manager Backend is LIVE 🚀";
-    }
+    // @GetMapping("/")
+    // public String home() {
+    //     return "Task Manager Backend is LIVE 🚀";
+    // }
 }
